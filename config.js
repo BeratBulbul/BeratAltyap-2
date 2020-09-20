4ce9e36d5decadaf96d314fadbf0205640403a8b
BeratBulbul <69976727+BeratBulbul@users.noreply.github.com>
2020-09-20 10:28:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
     "ownerID": "713831710885806125", //kendi IDınızı yazınız
     "admins": [""],
     "support": [""],
-    "token": "NzU1MzE3NjcyODMyNjYzNjMy.X2BiZg.EabmoAjV5wpSvVY-3snAVEL0obs", //botunuzun tokenini yazınız
+    "token": "", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "bOPKExCWkJs3PWSm9GwBlQpLL7EbLiyj", //botunuzun secretini yazınız
+      "oauthSecret": "", //botunuzun secretini yazınız
       "callbackURL": `callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "", //kalsın
       "domain": "", //site URLnizi yazınız!
